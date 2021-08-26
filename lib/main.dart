21e5bed8770d3ca117bfc41a0621a1b5f28ca1c6
Sumana Basu <sumana.basu2001@gmail.com>
2021-08-26 19:58:47 +0530

    Image and name

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,37 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text("Wanda's Profile Card"),
         centerTitle:true,
         backgroundColor: Colors.red[800],
         elevation:0.0,
-      )
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/wanda.jpeg'),
+                radius: 70.0,
+              ),
+            ),
+            Text(
+              'Wanda Maximoff',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                fontSize: 35.0,
+                fontFamily: 'Allison'
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
